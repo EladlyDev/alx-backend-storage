27395995d0d9d7ac7ab80e14be0ad69d773ae972
Mohamed <eladlydev@gmail.com>
2024-07-15 12:02:37 +0300

    Done with task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" returns the list of school having a specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Retrieve a list of schools that have a specific topic.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+        The MongoDB collection to query.
+        topic (str): The topic to search for.
+
+    Returns:
+        list: A list of school documents that have the specified topic.
+    """
+    return mongo_collection.find({'topics': topic})
