6ee59d3c5e2e57418eac1d8138f63bc5e7e2742c
Mohamed <eladlydev@gmail.com>
2024-07-15 11:31:59 +0300

    Done with task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" lists all documents """
+
+
+def list_all(mongo_collection):
+    """
+    Retrieve all documents from the given MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB
+        collection to retrieve documents from.
+
+    Returns:
+        list: A list of documents retrieved from the collection.
+        If no documents are found, an empty list is returned.
+    """
+    docs = mongo_collection.find()
+    return docs if docs else []
