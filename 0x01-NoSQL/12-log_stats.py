fdaa4e40c86936e99994863e8241572072c5e524
Mohamed <eladlydev@gmail.com>
2024-07-15 12:54:40 +0300

    Done with task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" provides some status about Nginx logs stored in mongodb """
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    client = MongoClient()
+    nginx_logs = client.logs.nginx.find()
+    out = dict(logs=0,
+               methods={'GET': 0, 'POST': 0,
+                        'PUT': 0, 'PATCH': 0, 'DELETE': 0},
+               status=0)
+
+    # fille out the data
+    for log in nginx_logs:
+        out['logs'] += 1
+        method = log.get('method')
+        if method in out['methods']:
+            out['methods'][method] += 1
+        if log.get('path', None) == '/status':
+            out['status'] += 1
+
+    # print the data in the required format
+    print(out['logs'], 'logs')
+    print('Methods:')
+    for k, v in out['methods'].items():
+        print(f'\tmethod {k}: {v}')
+    print(out['status'], 'status check')
