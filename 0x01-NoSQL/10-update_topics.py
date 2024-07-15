90147134721c362650e0d0f137b96569f07482be
Mohamed <eladlydev@gmail.com>
2024-07-15 11:51:23 +0300

    Done with taks 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" changes all topics of a school document """
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics of a school document in a MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The
+        MongoDB collection.
+        name (str): The name of the school document to update.
+        topics (list): The new list of topics for the school document.
+
+    Returns:
+        None
+    """
+    mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
