f75ea1d3bf5f177a89b0d065e8f3cdfddf5f9159
Mohamed <eladlydev@gmail.com>
2024-07-15 13:42:21 +0300

    Done with the second advanced task

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" returns all students sorted by average score """
+
+
+def top_students(mongo_collection):
+    """
+    Retrieves all students from the given MongoDB collection and returns them sorted by average score.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB collection containing student data.
+
+    Returns:
+        list: A list of dictionaries representing the students, sorted by average score.
+    """
+    res = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ])
+    return res
